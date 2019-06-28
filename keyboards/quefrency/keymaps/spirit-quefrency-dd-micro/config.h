c9fa2e987a016efe6f8096df5d667475d3cbf68f
mrweidai <mr.weidai@gmail.com>
2019-06-27 21:48:06 -0700

    Spirit48 Rev2

--- keyboards/quefrency/keymaps/spirit-quefrency-dd-micro/config.h
+++ keyboards/quefrency/keymaps/spirit-quefrency-dd-micro/config.h
@@ -26,9 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 // #define USE_I2C
 
-#define MOUSEKEY_DELAY                              300
+#define MOUSEKEY_DELAY                              30
 #define MOUSEKEY_INTERVAL                           50
-#define MOUSEKEY_MAX_SPEED                          10
-#define MOUSEKEY_TIME_TO_MAX                        20
+#define MOUSEKEY_MAX_SPEED                          20
+#define MOUSEKEY_TIME_TO_MAX                        10
 #define MOUSEKEY_WHEEL_MAX_SPEED                    8
 #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              40
--- keyboards/quefrency/keymaps/spirit-quefrency-dd-micro/keymap.c
+++ keyboards/quefrency/keymaps/spirit-quefrency-dd-micro/keymap.c
@@ -112,28 +112,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
     MO(_FN1),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  MO(_FN2),_______,_______, MO(_FN3),KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  MO(_FN2),KC_BSLS, _______,  MO(_FN3),KC_RCTL
   ),
 
   [_FN1] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSLS, \
-    _______, KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, _______,\
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, \
+    KC_TAB,  KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, _______, _______, _______, _______, KC_9,    KC_0,    KC_MINS, KC_EQL, KC_DEL, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, _______,\
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, \
     _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_FN2] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_GRV, \
-    _______, _______, _______, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______,\
-    _______, _______,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+    KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN1, _______, KC_GRV, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT, _______, \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, \
     _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_FN3] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_GESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, M(MACRO_EMOJI_SHRUG), MACRO_EMOJI_DAY, MACRO_EMOJI_FORBIDDEN, _______, _______, MACRO_EMOJI_HEART, _______, MACRO_EMOJI_KEYBOARD, _______, _______, _______,\
     _______, _______, MACRO_EMOJI_XOSS, MACRO_EMOJI_CHECK, _______, M(MACRO_EMOJI_BILL), _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
